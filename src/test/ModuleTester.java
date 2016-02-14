1d6ae6317dc96c1c3a010590010de9876a5957d3
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2016-02-13 17:54:00 -0700

    Testing for inverse

--- src/test/ModuleTester.java
+++ src/test/ModuleTester.java
@@ -4,14 +4,16 @@ import solver.Matrix;
 
 public class ModuleTester {
 	public static void main(String[] args) {
+		// int[][] a = { { 1, 2 }, { 3, 4 } };
 		// int[][] a = { { 10, 4, -1 }, { -1, 5, 4 }, { 5, -9, -6 } };
+		// int[][] a = { { -3, -2, -3 }, { 4, -7, -5 }, { 1, -8, -5 } };
 		int[][] a = { { -9, 1, 9, 8 }, { 0, 6, 2, 1 }, { 0, -30, -4, -6 },
 				{ 0, -24, 4, -13 } };
-		Matrix m = new Matrix(a);
-		System.out.println(m);
 
-		System.out.println("Determinant: " + m.getDeterminant());
-		//System.out.println(m.getAdjugate());
+		Matrix m = new Matrix(a);
+
+		System.out.println(m);
+		System.out.println(m.findInverse());
 	}
 }
 
