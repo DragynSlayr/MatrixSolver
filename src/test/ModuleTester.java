c8b2eba9a1499ee9c4976c88609d9740676d4ce5
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2016-02-16 10:30:58 -0700

    Added testing for transposition of vectors to columns

--- src/test/ModuleTester.java
+++ src/test/ModuleTester.java
@@ -18,23 +18,16 @@ public class ModuleTester {
 	 */
 	public static void main(String[] args) {
 		int[][] a = { { 1, 2, 3 }, { 4, 5, 6 } };
-		int[][] b = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-		int[][] c = { { 10, 4, -1 }, { -1, 5, 4 }, { 5, -9, -6 } };
-		int[][] d = { { -9, 1, 9, 8 }, { 0, 6, 2, 1 }, { 0, -30, -4, -6 } };
+		int[][] b = { { 1 }, { 2 } };
 
 		Matrix aMatrix = new Matrix(a);
 		Matrix bMatrix = new Matrix(b);
-		Matrix cMatrix = new Matrix(c);
-		Matrix dMatrix = new Matrix(d);
 
 		System.out.println(aMatrix);
 		System.out.println(bMatrix);
-		System.out.println(cMatrix);
-		System.out.println(dMatrix);
 
-		System.out.println(Matrix.multiplyMatrices(aMatrix, bMatrix));
-		System.out.println(Matrix.multiplyMatrices(bMatrix, aMatrix));
-		System.out.println(Matrix.multiplyMatrices(cMatrix, dMatrix));
-		System.out.println(Matrix.multiplyMatrices(dMatrix, cMatrix));
+		for (int i = 0; i < aMatrix.getColumns(); i++) {
+			System.out.println(Matrix.replaceColumn(aMatrix, bMatrix, i));
+		}
 	}
 }
