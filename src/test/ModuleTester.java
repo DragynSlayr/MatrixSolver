f98bce9c836dbd5a854f7a9c1eeef48a268bd435
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2016-02-15 23:36:36 -0700

    Testing for checking equality between two Matrix objects

--- src/test/ModuleTester.java
+++ src/test/ModuleTester.java
@@ -18,19 +18,15 @@ public class ModuleTester {
 	 */
 	public static void main(String[] args) {
 		int[][] a = { { 1, 2 }, { 3, 4 } };
-		int[][] b = { { 9, 8 }, { 7, 6 } };
+		int[][] b = { { 1, 2 }, { 3, 5 } };
 		// int[][] a = { { 10, 4, -1 }, { -1, 5, 4 }, { 5, -9, -6 } };
 		// int[][] a = { { -9, 1, 9, 8 }, { 0, 6, 2, 1 }, { 0, -30, -4, -6 },
 		// { 0, -24, 4, -13 } };
 
 		Matrix m = new Matrix(a);
-		System.out.println(m);
-
 		Matrix n = new Matrix(b);
-		System.out.println(n);
 
-		System.out.println(Matrix.addMatrices(m, n));
-		System.out.println(Matrix.subtractMatrices(m, n));
-		System.out.println(Matrix.subtractMatrices(n, m));
+		System.out.println(m.equals(n));
+		System.out.println(n.equals(m));
 	}
 }
