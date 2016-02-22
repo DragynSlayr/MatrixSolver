7637807a3766537d8d9420ddd9cb6f813cc8f48e
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2016-02-21 18:35:18 -0700

    Testing for Exponential class

--- src/test/ModuleTester.java
+++ src/test/ModuleTester.java
@@ -1,7 +1,7 @@
 package test;
 
+import solver.Exponential;
 import solver.Fraction;
-import solver.Matrix;
 
 /**
  * A class for testing aspects of the MatrixSolver and supporting classes
@@ -18,25 +18,9 @@ public class ModuleTester {
 	 *            Command line arguments, unsupported
 	 */
 	public static void main(String[] args) {
+		Exponential exp = new Exponential(new Fraction(1, 2), new Fraction(-1,
+				2));
 
-		// int[][] a = { { 3, 9, 2 }, { 1, 1, 0 }, { 2, 1, -1 } };
-		Fraction[][] a = {
-				{ new Fraction(1, 2), new Fraction(7, 3), new Fraction(2) },
-				{ new Fraction(0), new Fraction(1), new Fraction(-1) },
-				{ new Fraction(10, 3), new Fraction(42), new Fraction(-3) } };
-		int[][] b = { { 21 }, { 2 }, { 1 } };
-		// int[][] a = { { 3, 2, 1 }, { 9, 3, 6 }, { 1, 2, 4 } };
-
-		Matrix aMatrix = new Matrix(a);
-		System.out.println(aMatrix);
-
-		Matrix bMatrix = new Matrix(b);
-		System.out.println(bMatrix);
-
-		Fraction[] solutions = Matrix.findSolution(aMatrix, bMatrix);
-
-		for (int i = 0; i < solutions.length; i++) {
-			System.out.printf("X%d: %s\n", i + 1, solutions[i]);
-		}
+		System.out.println(exp);
 	}
 }
